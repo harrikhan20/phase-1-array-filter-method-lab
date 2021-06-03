2c72c7369df524fe39b68d1b65827e21a4d0bcc8
harris <khanharris7@yahoo.com>
2021-06-02 19:25:36 -0500

    commit tree ea0108e403831a68d6ebfc169055c57667397b5c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverArray, name){
+return driverArray.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(driverArray, name){
+    return driverArray.filter(person => person[0].toLowerCase() === name[0].toLowerCase())
+}
+
+function matchName(driverArray, name){
+    return driverArray.filter(person => person.name === name)
+}
